663702465ba36ada6d7953cb929e17c98662d45d
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-12 13:11:54 +0800

    Remove JKR references

--- src/app/service-requisition/service-requisition.module.ts
+++ src/app/service-requisition/service-requisition.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
 import { CommonModule } from '@angular/common';
 import { ServiceRequisitionComponent } from './service-requisition.component';
 import { ServiceRequisitionRoutingModule } from './service-requisition-routing.module';
+import { ImageUploadModule } from 'angular2-image-upload';
 
 @NgModule({
   imports: [
-    ServiceRequisitionRoutingModule
+    HttpModule,
+    ServiceRequisitionRoutingModule,
+    ImageUploadModule.forRoot()
   ],
   declarations: [ServiceRequisitionComponent]
 })
