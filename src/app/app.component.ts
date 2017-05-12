7108aa091e67eddb89fb34412294b1ec95b53fb7
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-12 13:33:22 +0800

    Scroll back top on page change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   // tslint:disable-next-line
   selector: 'body',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+
+      var scrollToTop = window.setInterval(function () {
+        var pos = window.pageYOffset;
+        if (pos > 0) {
+          window.scrollTo(0, pos - 20); // how far to scroll on each step
+        } else {
+          window.clearInterval(scrollToTop);
+        }
+      }, 16); // how fast to scroll (this equals roughly 60 fps)
+    });
+  }
+}
