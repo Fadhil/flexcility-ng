a6b7b0d99eb3e03f412b5dc8453a36661387247e
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-27 05:28:42 +0800

    Add toggle stuff for Helpdesk layout

--- src/app/layouts/helpdesk-layout/helpdesk-layout.component.ts
+++ src/app/layouts/helpdesk-layout/helpdesk-layout.component.ts
@@ -8,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HelpdeskLayoutComponent implements OnInit {
 
   constructor() { }
+  public disabled = false;
+  public status: {isopen: boolean} = {isopen: false};
+
+  public toggled(open: boolean): void {
+    console.log('Dropdown is now: ', open);
+  }
+
+  public toggleDropdown($event: MouseEvent): void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
+  }
 
   ngOnInit() {
 
