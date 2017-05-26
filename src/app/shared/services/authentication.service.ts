a7536e6081cc1df254ce5f81a523da05947f08bc
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-26 10:11:31 +0800

    Fix authentication service app.config dir

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -3,7 +3,7 @@ import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 
-import { AppConfig } from '../app.config';
+import { AppConfig } from '../../app.config';
 
 @Injectable()
 export class AuthenticationService {
