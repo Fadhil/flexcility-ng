f6b7fbe0a47a9c1f95f8b1f26772e53305b69ed3
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-26 10:11:08 +0800

    Add User model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,7 @@
-/**
- * Created by fadhil on 25/05/2017.
- */
 export class User {
-  constructor(
-    public id: string,
-    public name: string,
-    public email: string,
-    public password: string,
-  )
+  _id: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
 }
