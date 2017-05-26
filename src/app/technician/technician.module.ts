8fdc295e03bf68636fab25b83ce6c1caa2008603
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-27 02:29:11 +0800

    Technician Dashboard WIP

--- src/app/technician/technician.module.ts
+++ src/app/technician/technician.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {TechnicianRoutingModule} from "./technician-routing.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    TechnicianRoutingModule
+    TechnicianRoutingModule,
+    ModalModule.forRoot()
+
   ],
   declarations: [DashboardComponent]
 })
