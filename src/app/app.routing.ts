dd2c5b6b98a1c01a0d334156bd07f4d8e2f9992a
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-05-31 09:13:03 +0800

    Remove auth guard temporarily

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,7 +17,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    component: FullLayoutComponent, canActivate: [AuthGuard],
+    component: FullLayoutComponent,
     data: {
       title: 'Home'
     },
