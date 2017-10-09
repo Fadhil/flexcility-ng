e027a179e3f235f4019717f3dec96b7e63c8a295
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-10-09 21:41:12 +0800

    Add basic Site model

---
+++ src/app/models/site.ts
@@ -0,0 +1,18 @@
+export class Site {
+    _id: number;
+    name: string;
+    address: string;
+    description: string;
+
+    constructor(
+        // public _id: number,
+        // public name: string,
+        // public address: string,
+        // public description: string,
+    ){
+        this._id = null;
+        this.name = null;
+        this.address = null;
+        this.description = null;
+    }
+}
