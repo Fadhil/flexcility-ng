10352bc84ced263c114aef2e0c2b21d2e4fe5214
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-10-09 21:44:51 +0800

    Build basic site form in AdminSiteNewComponent
    * Add createForm()
    * Run createForm in constructor
    * Add onSubmit()

--- src/app/admin/admin-site/admin-site-new/admin-site-new.component.ts
+++ src/app/admin/admin-site/admin-site-new/admin-site-new.component.ts
@@ -1,4 +1,7 @@
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Site } from '../../../models/site';
 
 @Component({
   selector: 'app-admin-site-new',
@@ -7,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class AdminSiteNewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  siteForm: FormGroup;
+  submitted = false;
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
   }
 
+  ngOnInit() {
+
+  }
+
+  createForm(){
+    this.siteForm = this.fb.group({
+      name: ['', Validators.required ],
+      address: '',
+      description: ''
+    });
+  }
+
+  onSubmit() { this.submitted = true; console.log("submitted:", this.submitted); }
+
+  // get diagnostic() { return JSON.stringify(this.model); }
 }
